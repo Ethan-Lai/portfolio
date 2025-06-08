c110ec6d5e807150168018d0e120626875289862
Ethan Lai <ethanc.lai@mail.utoronto.ca>
2025-06-08 19:22:49 -0400

    Improved accessibility for hero section

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -23,28 +23,28 @@ const Blog = () => {
     // title, date, tags, general, code, leetcode
 
     return (
-      <div>
-        <h1>My Blog</h1>
+          <div>
+            <h1>My Blog</h1>
 
-        {posts.map((post) => (
-          <div key={post.sys.id}>
-            <h2>{post.fields.title}</h2>
-            <p>
-                <strong>Date:</strong>{" "}
-                {new Date(post.fields.date).toLocaleDateString()}
-            </p>
-            <p>
-              <strong>Tags:</strong> {post.fields.tags}
-            </p>
-            <h1>General</h1>
-            <div>{post.fields.general}</div>
-            <h1>Code</h1>
-            <div>{post.fields.code}</div>
-            <h1>Leetcode</h1>
-            <div>{post.fields.leetcode}</div>
-          </div>
-        ))}
-      </div>
+                {posts.map((post) => (
+                    <div key={post.sys.id}>
+                            <h2>{post.fields.title}</h2>
+                            <p>
+                                <strong>Date:</strong>{" "}
+                                {new Date(post.fields.date).toLocaleDateString()}
+                            </p>
+                            <p>
+                            <strong>Tags:</strong> {post.fields.tags}
+                            </p>
+                            <h1>General</h1>
+                            <div>{post.fields.general}</div>
+                            <h1>Code</h1>
+                            <div>{post.fields.code}</div>
+                            <h1>Leetcode</h1>
+                            <div>{post.fields.leetcode}</div>
+                    </div>
+                ))}
+        </div>
     );
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,24 +25,27 @@ const Hero= () => {
         <div className="flex w-full justify-between">
             <div>
                 <div className="flex flex-col gap-2">
-                    <h1 className="text-3xl font-bold">Hi, ethan here</h1>
-                    <p>21 year old software developer from Canada 🇨🇦</p>
-                    <p>I like to develop full-stack, play sports, and learn new things.</p>
-                    <strong>Any questions? Feel free to email me!</strong>
+                    <h1 className="title text-balance text-4xl sm:text-5xl">hi ethan here. 👋</h1>
+                    <p className="mt-2 whitespace-nowrap text-sm font-medium sm:text-base">21 year old software developer from Canada 🇨🇦</p>
+                    <p className="mt-4 max-w-sm text-balance text-sm sm:text-base">I like to develop full-stack, play sports, and learn new things.</p>
+                    <p className="text-balance text-sm font-semibold sm:text-base"><strong>Any questions? Feel free to email me!</strong></p>
                 </div>
 
                 <div className="flex items-center mr-5 gap-4 mt-2">
-                    <Button onClick={downloadResume}>
+                    <Button onClick={downloadResume} className="cursor-pointer">
                         Resume
                         <FontAwesomeIcon icon={faDownload} />
                     </Button>   
                     <a href="https://www.linkedin.com/in/ethan-lai-2b008221a/">
+                        <span className="sr-only">Link to LinkedIn</span>
                         <FontAwesomeIcon icon={faLinkedinIn} className="text-xl" />
                     </a>
                     <a href="https://github.com/Ethan-Lai">
+                        <span className="sr-only">Link to Github</span>
                         <FontAwesomeIcon icon={faGithub} className="text-xl" />
                     </a>
-                    <a onClick={copyEmail}>
+                    <a onClick={copyEmail} className="cursor-pointer">
+                        <span className="sr-only">Copy email</span>
                         <FontAwesomeIcon icon={faEnvelope} className="text-xl" />
                     </a>
                 </div>
