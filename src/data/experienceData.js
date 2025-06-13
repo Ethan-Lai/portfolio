f074d624eb63b0f2c7b9ebb7652f13410cdfe385
Ethan Lai <ethanc.lai@mail.utoronto.ca>
2025-06-13 12:00:25 -0400

    refactor: reorganize components for better maintainability

---
+++ src/data/experienceData.js
@@ -0,0 +1,54 @@
+export const workExperience = [
+    {
+        id: 1,
+        logo: "/organizations/OneChartLogo.jpeg",
+        logoAlt: "OneChart Logo",
+        website: "https://onechart.ai/",
+        startDate: "May 2024",
+        endDate: "May 2025",
+        organization: "OneChart",
+        role: "Software Developer",
+        description: [
+            "Developed an automated React form builder to streamline client onboarding, supporting 1,500+ users and reducing development time by 100+ hours.",
+            "Built reusable React component library for form builder, reducing new form type development time by 20+ hours and supporting 10+ customizable UI elements.",
+            "Deployed and maintained Docker instances of OneChart on MedStack cluster, serving 10 healthcare organizations with secure cloud infrastructure.",
+            "Implemented Stripe API to automate subscription billing, handling 1,500+ checkout transactions.",
+            "Developed Celery (Python) workers using Redis to manage AI dictation workflows, eliminating API bottlenecks and improving system response time by 20%."
+        ]
+    },
+    {
+        id: 2,
+        logo: "/organizations/CMALogo.jpeg",
+        logoAlt: "Champions Martial Arts Logo",
+        website: "https://www.championsmartialarts.ca/",
+        startDate: "April 2024",
+        endDate: "March 2025",
+        organization: "Champions Martial Arts",
+        role: "Martial Arts Instructor",
+        description: [
+            "Broke down complex martial arts techniques into manageable steps, reducing average skill mastery time by 50%",
+            "Adapted teaching methods to accommodate students with diverse learning styles and physical abilities, achieving 80% student retention",
+        ]
+    }
+]
+
+export const educationExperience = [
+    {
+        id: 1,
+        logo: "/organizations/UTMLogo.jpeg",
+        logoAlt: "University of Toronto Mississauga Logo",
+        website: "https://www.utm.utoronto.ca/",
+        startDate: "September 2021",
+        endDate: "April 2026",
+        organization: "University of Toronto Mississauga",
+        role: "Bachelor's of Science in Economics",
+        description: [
+            "Computer Science Minor",
+            "Mathematical Science Minor"
+        ]
+    }
+]
+
+export const toolsExperience = [
+    
+]
