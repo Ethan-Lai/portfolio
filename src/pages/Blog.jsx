171ab5f6046a2f622c1946db195b06d76e6db83e
Ethan Lai <ethanc.lai@mail.utoronto.ca>
2025-06-21 12:52:17 -0400

    Added easier movement between pages with view more button

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react"
 import { client } from "../client"
 import BlogItem from "@/components/BlogItem"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate } from "react-router-dom"
 
 const Blog = ({ limit }) => {
+    const navigate = useNavigate()
     const [posts, setPosts] = useState([])
     const [loading, setLoading] = useState(true);
 
@@ -23,7 +27,16 @@ const Blog = ({ limit }) => {
 
     return (
         <div>
-            <h1 className={`title text-balance ${limit ? "text-4xl" : "text-5xl"} calistoga-regular`}>{limit ? "recent posts" : "blog"}</h1>
+            <div className="flex justify-between items-center">
+                <h1 className={`title text-balance ${limit ? "text-4xl" : "text-5xl"} calistoga-regular`}>{limit ? "recent posts" : "blog"}</h1>
+                {
+                    limit &&
+                    <div className="flex items-center gap-2 cursor-pointer" onClick={() => navigate("/blog")}>
+                        <span>view more</span>
+                        <FontAwesomeIcon icon={faArrowRight} />
+                    </div>
+                }            
+            </div>
             <br />
             {posts.map(post => (
                 <BlogItem key={post.sys.id} post={post} />
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,13 @@
 import React from "react"
 import { projectsExperience } from "../data/projectData"
 import ProjectItem from "../components/ProjectItem"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate } from "react-router-dom"
 
 const Projects = ({ limit }) => {
+    const navigate = useNavigate()
+    
     const projects = (limit ? projectsExperience.slice(0, limit) : projectsExperience)
         .map(project => 
             <ProjectItem project={project} />
@@ -10,7 +15,16 @@ const Projects = ({ limit }) => {
 
     return (
         <div className="flex flex-col w-full">
-            <h1 className={`title text-balance ${limit ? "text-4xl" : "text-5xl"} calistoga-regular`}>{ limit ? "featured projects" : "projects"}</h1>
+            <div className="flex justify-between items-center"> 
+                <h1 className={`title text-balance ${limit ? "text-4xl" : "text-5xl"} calistoga-regular`}>{ limit ? "featured projects" : "projects"}</h1>
+                {
+                    limit &&
+                    <div className="flex items-center gap-2 cursor-pointer" onClick={() => navigate("/projects")}>
+                        <span>view more</span>
+                        <FontAwesomeIcon icon={faArrowRight} />
+                    </div>
+                }
+            </div>
             <br />
             <div className="grid grid-col-1 sm:grid-cols-2 gap-2">
                 {projects}
